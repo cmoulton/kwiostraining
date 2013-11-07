f6a20f735fb00242c986790aff0705cd48a06803
Christina <christina.moulton@gmail.com>
2013-11-07 11:18:48 -0500

    Created fruits & veg demo project (empty)

---
+++ tableview exercise/FruitVsVeg/FruitVsVeg/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FruitVsVeg
+//
+//  Created by Christina Moulton on 11/7/2013.
+//  Copyright (c) 2013 Teak Mobile Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
---
+++ tableview exercise/FruitVsVeg/FruitVsVegTests/FruitVsVegTests.m
@@ -0,0 +1,34 @@
+//
+//  FruitVsVegTests.m
+//  FruitVsVegTests
+//
+//  Created by Christina Moulton on 11/7/2013.
+//  Copyright (c) 2013 Teak Mobile Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface FruitVsVegTests : XCTestCase
+
+@end
+
+@implementation FruitVsVegTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
