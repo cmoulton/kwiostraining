27fce211d33e733ec5245f6f4c213aa712ccf4a4
Christina <christina.moulton@gmail.com>
2013-11-07 11:24:46 -0500

    Added custom class for tableview controller

---
+++ tableview exercise/FruitVsVeg/FruitVsVeg/FruitVsVegViewController.h
@@ -0,0 +1,13 @@
+//
+//  FruitVsVegViewController.h
+//  FruitVsVeg
+//
+//  Created by Christina Moulton on 11/7/2013.
+//  Copyright (c) 2013 Teak Mobile Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FruitVsVegViewController : UITableViewController
+
+@end
